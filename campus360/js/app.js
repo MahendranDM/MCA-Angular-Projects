6e8316b49dbeab46d12b8cd7d7412d1a5731a5fa
MahendranDM <mahendrandm2003@gmail.com>
2026-07-21 11:43:28 +0530

    fix: correct totalStudents variable casing and track by index in student list ng-repeat

--- campus360/js/app.js
+++ campus360/js/app.js
@@ -1,15 +1,24 @@
 var app = angular.module('campus360', []);
 
-
 app.controller("DashboardController", function ($scope) {
 
-    $scope.collegename = "FISAT"
-    $scope.name = "Mahendran DM"
-    $scope.city = "Kollam"
-    $scope.age = "23"
-
+    $scope.collegename = "FISAT";
+    $scope.name = "Mahendran DM";
+    $scope.city = "Kollam";
+    $scope.age = 23;
+    $scope.totalStudents = 1230;
 
     console.log("Dashboard Controller is loaded");
 
 
-})
+    $scope.students = [
+        "Mahendran DM","Anandhu", "Akhil", "Athul", "Anandhu"];
+
+
+        $scope.addStudent = function () {
+
+            $scope.totalStudents++;
+
+
+        };
+});
